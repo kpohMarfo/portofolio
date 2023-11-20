77d45a7d3c7adeeab7033c379eecd5c85f6cb3d9
kpohMarfo <kpohmarfo@gmail.com>
2023-11-21 01:47:01 +0700

    first commit

---
+++ portofolio/resources/views/home.blade.php
@@ -0,0 +1,278 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Portofolio Marfo Kapoh </title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="css/animate.css">
+	<link rel="stylesheet" href="css/flexslider.css">
+	<link rel="stylesheet" href="fonts/icomoon/style.css">
+
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/style.css">
+
+	<link href="https://fonts.googleapis.com/css?family=Nunito+Sans:200,300,400,700" rel="stylesheet">
+
+
+</head>
+<body data-spy="scroll" data-target="#pb-navbar" data-offset="200">
+
+
+
+	<nav class="navbar navbar-expand-lg site-navbar navbar-light bg-light" id="pb-navbar">
+
+		<div class="container">
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+
+
+			<div class="collapse navbar-collapse justify-content-md-center" id="navbarsExample09">
+				<ul class="navbar-nav">
+					<li class="nav-item"><a class="nav-link" href="#section-home">Home</a></li>
+					<li class="nav-item"><a class="nav-link" href="#section-portfolio">Portfolio</a></li>
+					<li class="nav-item"><a class="nav-link" href="#section-about">About</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+
+
+
+
+	<section class="site-hero" style="background-image: url(images/p1bg.jpeg);" id="section-home" data-stellar-background-ratio="0.5">
+		<div class="container">
+			<div class="row intro-text align-items-center justify-content-center">
+				<div class="col-md-10 text-center pt-5">
+
+					<h1 class="site-heading site-animate">Hai!, I am  <strong class="d-block">Marfo Kapoh</strong></h1>
+					<strong class="d-block text-white text-uppercase letter-spacing">Software Engineer </strong>
+
+				</div>
+			</div>
+		</div>
+	</section> <!-- section -->
+
+
+
+
+
+
+	<section class="site-section" id="section-portfolio">
+		<div class="container">
+			<div class="row">
+				<div class="section-heading text-center col-md-12">
+					<h2><strong>Portfolio</strong></h2>
+				</div>
+			</div>
+			<div class="filters">
+				<ul>
+					<li class="active" data-filter="*">All</li>
+					<li data-filter=".gamedev">Game Developer</li>
+					<li data-filter=".mobiledev">Mobile Developer</li>
+					<li data-filter=".webdev">Web Developoer</li>
+				</ul>
+			</div>
+
+			<div class="filters-content">
+				<div class="row grid">
+					<div class="single-portfolio col-sm-4 all gamedev">
+						<div class="relative">
+							<div class="thumb">
+								<div class="overlay overlay-bg"></div>
+								<img class="image img-fluid" src="images/p1bg.jpeg" alt="">
+							</div>                         
+						</div>
+					<div class="p-inner">
+							<h4>Maze Runner</h4>
+							<div class="cat">C# & UNITY Engine</div>
+						</div>                                         
+					</div>
+					<div class="single-portfolio col-sm-4 all gamedev">
+						<div class="relative">
+							<div class="thumb">
+								<div class="overlay overlay-bg"></div>
+								<img class="image img-fluid" src="images/p1bg.jpeg" alt="">
+							</div>
+                                
+						</div>
+						<div class="p-inner">
+							<h4>Techoverse</h4>
+							<div class="cat">C#, PHOTON NETWORK & UNITY Engine</div>
+						</div>                                         
+					</div>                            
+					<div class="single-portfolio col-sm-4 all webdev">
+						<div class="relative">
+							<div class="thumb">
+								<div class="overlay overlay-bg"></div>
+								<img class="image img-fluid" src="images/p1bg.jpeg" alt="">
+							</div>
+						</div>
+						<div class="p-inner">
+							<h4>To do List</h4>
+							<div class="cat">HTML, CSS, JS & PHP</div>
+						</div>
+					</div>
+					<div class="single-portfolio col-sm-4 all mobiledev">
+						<div class="relative">
+							<div class="thumb">
+								<div class="overlay overlay-bg"></div>
+								<img class="image img-fluid" src="images/p1bg.jpeg" alt="">
+							</div>                           
+						</div> 
+						<div class="p-inner">
+							<h4>Perhitungan nilai</h4>
+							<div class="cat">JAVA</div>
+						</div>
+					</div>
+					<div class="single-portfolio col-sm-4 all mobiledev">
+						<div class="relative">
+							<div class="thumb">
+								<div class="overlay overlay-bg"></div>
+								<img class="image img-fluid" src="images/p1bg.jpeg" alt="">
+							</div>                              
+						</div>
+						<div class="p-inner">
+							<h4>CHATAPP</h4>
+							<div class="cat">FLUTTER, GETX & FIREBASE</div>
+						</div>
+					</div>
+					<div class="single-portfolio col-sm-4 all mobiledev">
+						<div class="relative">
+							<div class="thumb">
+								<div class="overlay overlay-bg"></div>
+								<img class="image img-fluid" src="images/p1bg.jpeg" alt="">
+							</div>                      
+						</div>
+						<div class="p-inner">
+							<h4>Metric Converter</h4>
+							<div class="cat">IONIC & REACT</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- .section -->
+
+	
+	<section class="site-section " id="section-resume">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 mb-5">
+					<div class="section-heading text-center">
+						<h2>Pengalaman <strong>saya</strong></h2>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<h2 class="mb-5">Organisasi</h2>
+					<div class="resume-item mb-4">
+						<span class="date"><span class="icon-calendar"></span> March 2013 - Present</span>
+						<h3>Anggota Bidang Advokasi & Kesejahteraan Mahasiswa</h3>
+						<p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+						<span class="school">BEM FT UNSRAT</span>
+					</div>
+
+					<div class="resume-item mb-4">
+						<span class="date"><span class="icon-calendar"></span> March 2013 - Present Deacember.</span>
+						<h3>Anggota Bidang Kreativitas Mahasiswa</h3>
+						<p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+						<span class="school">UKM ESPORT FT UNSRAT</span>
+					</div>
+
+					<div class="resume-item mb-4">
+						<span class="date"><span class="icon-calendar"></span> March 2013 - Present</span>
+						<h3>Koordinator Bidang Hubungan Masyarakat & Kemitraan</h3>
+						<p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+						<span class="school">New York University</span>
+					</div>
+
+					<div class="resume-item mb-4">
+						<span class="date"><span class="icon-calendar"></span> March 2013 - Present Deacember.</span>
+						<h3>Anggota UNSRAT IT Comunnity</h3>
+						<p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+						<span class="school">UNSRAT IT Comunnity</span>
+					</div>
+
+				</div>
+				<div class="col-md-6">
+
+
+					<h2 class="mb-5">Pekerjaan/Volunteer</h2>
+
+					<div class="resume-item mb-4">
+						<span class="date"><span class="icon-calendar"></span> March 2013 - Present</span>
+						<h3>Asisten Dosen</h3>
+						<p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+						<span class="school">Universitas Sam Ratulangi</span>
+					</div>
+
+					<div class="resume-item mb-4">
+						<span class="date"><span class="icon-calendar"></span> March 2013 - Present</span>
+						<h3>Promotor</h3>
+						<p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+						<span class="school">PT. Intrias Mandiri Sejati</span>
+					</div>
+
+					<div class="resume-item mb-4">
+						<span class="date"><span class="icon-calendar"></span> March 2013 - Present</span>
+						<h3>Mobile Developer (intern) </h3>
+						<p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+						<span class="school">PT.Aplikasi UNIQ Indonesia</span>
+					</div>
+
+					<div class="resume-item mb-4">
+						<span class="date"><span class="icon-calendar"></span> March 2013 - Present</span>
+						<h3>UNITY Developer</h3>
+						<p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+						<span class="school">UNSRAT IT Community</span>
+					</div>
+
+
+				</div>
+			</div>
+		</div>
+	</section> <!-- .section -->
+
+	<section class="site-section" id="section-about">
+		<div class="container">
+			<div class="row mb-5 align-items-center">
+				<div class="col-lg-7 pr-lg-5 mb-5 mb-lg-0">
+					<img src="images/image_1.jpg" alt="Image placeholder" class="img-fluid">S
+				</div>
+				<div class="col-lg-5 pl-lg-5">
+					<div class="section-heading">
+						<h2>Tentang<strong>saya</strong></h2>
+					</div>
+					<p class="lead">Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+					<p class="mb-5  ">A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
+
+					<p>
+						<a href="https://api.whatsapp.com/send/?phone=621240621907&text&type=phone_number&app_absent=0" class="btn btn-primary px-4 py-2 btn-sm smoothscroll">Contact Me</a>
+					</p>
+				</div>
+			</div>
+
+
+		</div>
+	</section>
+
+
+
+
+	<script src="js/vendor/jquery.min.js"></script>
+	<script src="js/vendor/jquery-migrate-3.0.1.min.js"></script>
+	<script src="js/vendor/popper.min.js"></script>
+	<script src="js/vendor/bootstrap.min.js"></script>
+
+	<script src="js/vendor/jquery.easing.1.3.js"></script>
+
+	<script src="js/vendor/jquery.stellar.min.js"></script>
+	<script src="js/vendor/jquery.waypoints.min.js"></script>
+
+	<script src="https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.min.js"></script>
+	<script src="https://unpkg.com/imagesloaded@4/imagesloaded.pkgd.min.js"></script>
+	<script src="js/custom.js"></script>
+
+    </body>
+    </html>
